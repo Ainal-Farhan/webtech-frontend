2bb0c5a25b963bf61146eddcb5ed1387fbeb8b31
Ainal-Farhan <mohd.ainalfarhan7@gmail.com>
2021-07-21 15:30:02 +0800

    all done

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,8 +2,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { environment } from '@environments/environment';
-import { User } from '../../models/user';
+import { environment } from '../../environments/environment';
+import { User } from '../models/user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
